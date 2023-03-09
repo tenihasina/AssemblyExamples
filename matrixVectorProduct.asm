e651ecb20d07c6198cd28366e64480b765cb3853
tenihasina <sandratrarasendrasoa@gmail.com>
2023-03-09 15:20:18 +0100

    WIP, bad results

--- matrixVectorProduct.asm
+++ matrixVectorProduct.asm
@@ -12,9 +12,30 @@ debut:
     mov ax, data
     mov ds, ax
     
-
-    
+    xor si, si
+    xor bp, bp
+    mov cl, taille
+    dec cx
+full_matrix:
+    xor bx, bx
+dot_product:
+    mov al, mat[bp][si]
+    mul vec[si]; vec1[i]*vec2[i]
+    ; on additionne le résultat de la multiplication à bx
+    add bx, ax
+    ; si jamais on gère des words et non des bytes
+    add bx, dx; 
+    ; incrément indice
+    inc si
+    cmp si, cx
+    jne dot_product
+    ;ecriture memoire après chaque produit scalaire
+    mov res[bp], bx
+    inc bp
+    cmp bp, cx 
+    jne full_matrix
 fin:
     mov ax, 4ch
     int 21h
 code ends
+    end debut
