6632c487cbb69db8e04b0d194c2764baa53b8acb
tenihasina <sandratrarasendrasoa@gmail.com>
2023-03-09 14:54:36 +0100

    smaller example

--- matrixVectorProduct.asm
+++ matrixVectorProduct.asm
@@ -1,16 +1,18 @@
 assume cs:code, ds:data
 
 data segment
-    mat db 100 dup(?)
-    vec db 10 dup(?)
-    taille 10
-    res dw 10 dup(0)
+    taille db 3
+    mat db 1,2,3,4,5,6,7,8,9
+    vec db 2,3,4
+    res dw 3 dup(0)
 data ends
 
 code segment
 debut:
     mov ax, data
     mov ds, ax
+    
+
     
 fin:
     mov ax, 4ch
