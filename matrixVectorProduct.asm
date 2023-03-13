1229e100dd8767c2f0bceb1ecf6c5c29d84aabe4
tenihasina <sandratrarasendrasoa@gmail.com>
2023-03-13 15:51:52 +0100

    product latrix vector working

--- matrixVectorProduct.asm
+++ matrixVectorProduct.asm
@@ -1,5 +1,5 @@
 assume cs:code, ds:data
-
+; 14, 2F, 4A
 data segment
     taille db 3
     mat db 1,2,3,4,5,6,7,8,9
@@ -11,28 +11,44 @@ code segment
 debut:
     mov ax, data
     mov ds, ax
-    
     xor si, si
     xor bp, bp
     mov cl, taille
-    dec cx
+
 full_matrix:
+    xor di, di
     xor bx, bx
+    xor ax, ax
+    lea bp, mat
 dot_product:
-    mov al, mat[bp][si]
-    mul vec[si]; vec1[i]*vec2[i]
+    xor ax, ax
+    xor dx, dx
+    mov al, ds:[bp][si]
+    mul byte ptr vec[di]; vec1[i]*vec2[i]
     ; on additionne le résultat de la multiplication à bx
-    add bx, ax
+    add bx, ax 
     ; si jamais on gère des words et non des bytes
-    add bx, dx; 
+    add bx, dx 
     ; incrément indice
     inc si
-    cmp si, cx
+    inc di
+    cmp di, cx
     jne dot_product
-    ;ecriture memoire après chaque produit scalaire
-    mov res[bp], bx
-    inc bp
-    cmp bp, cx 
+; emplacement mémoire indiqué par si
+calcul_index_ecriture:
+    xor ax, ax
+    xor bp, bp
+    mov ax, si
+    div cl
+    xor ah, ah; on ne veut pas du reste de la division
+    mov bp, ax; emplacement pour des words, donc 2 cases mémoires
+    dec bp
+    add bp, bp
+ecriture:
+    mov res[bp], bx 
+    ;xor dx, dx
+    ; inc bp   
+    cmp ax, cx 
     jne full_matrix
 fin:
     mov ax, 4ch
